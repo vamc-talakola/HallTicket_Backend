387af26f434eac9ae71a5b501db78a7bc8697fe2
vamc-talakola <vamsitalakola7777@gmail.com>
2025-01-06 20:48:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -33,6 +33,13 @@ app.use(express.urlencoded({ extended: false }));
 // };
 
 app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+  next();
+});
 const nodemailer = require('nodemailer');
 
 const sendEmail = (to, subject, text) => {
